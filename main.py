0003391f5771b107bfa4bfe7f2f1c8ed5aa3df5e
neighdeen84 <60201315+neighdeen84@users.noreply.github.com>
2021-02-04 18:11:59 -0800

    subplots yay

--- main.py
+++ main.py
@@ -20,33 +20,42 @@ WH = pd.read_csv("wh1.csv", usecols=col_list)
 df_output = pd.DataFrame(data=output)
 df_WH = pd.DataFrame(data = WH)
 
-#print(df1)
-
-
-
-
-#print(df_output)
-
-# Naming the columns:
-#df_output.columns = ['Time', 'Power', 'unwanted_col_1', 'unwanted_col_2']
+####################################################################################
+# Cleaning up df_output (not necessary if you don't open in excel):
+# Naming the columns for df_output:
+df_output.columns = ['Time', 'Power', 'unwanted_col_1', 'unwanted_col_2']
 
 # Popping the last two NaN columns (not sure why this happened, will investigate):
-#df_output.pop("unwanted_col_1")
-#df_output.pop("unwanted_col_2")
+df_output.pop("unwanted_col_1")
+df_output.pop("unwanted_col_2")
 
 # Getting rid of PST:
-#df_output['Time'] = df_output['Time'].str.replace("PST", "")
+df_output['Time'] = df_output['Time'].str.replace("PST", "")
 
 # Checking for NaN values:
-#print(df_output.isnull().values.any())
+print(df_output.isnull().values.any())
 
-# Printing the data:
-#print(df_output)
+####################################################################################
 
+# Printing dataframes:
+print(df_output)
+print(df_WH)
 
 # Plotting:
+# Power vs time:
 #df_output.plot(x ='Time', y='Power', kind = 'line')
 #plt.show()
 
-df_WH.plot(x ='# timestamp', y=' temperature', kind = 'line')
+#df_output.plot(x ='Time', y='Power', kind = 'line', subplots=True, layout=(2,1,))
+#df_WH.plot(x ='# timestamp', y=' temperature', kind = 'line', subplots=True, layout=(2,1))
+
+# Temperature vs time:
+#df_WH.plot(x ='# timestamp', y=' temperature', kind = 'line')
+#plt.show()
+
+# Plotting using subplots:
+figure, axes = plt.subplots(2, 1)
+df_output.plot(x ='Time', y='Power', title ='Power vs Time', kind = 'line', ax=axes[0])
+
+df_WH.plot(x ='# timestamp', y=' temperature', title ='Temperature vs Time',  kind = 'line', ax=axes[1])
 plt.show()
